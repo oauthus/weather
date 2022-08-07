9da058085f70d7c47f7d4a9ffa5194c9a5e55375
Verdandi <var.verdandi@gmail.com>
2022-08-07 18:17:15 +0400

    final pack

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,8 +1,7 @@
-
 class WeatherDisplay extends Component {
-    render() {
-      return (
-        <h1>Displaying weather for city {this.props.zip}</h1>
-      );
-    }
+  render() {
+    return (
+      <h1>Displaying weather for city {this.props.zip}</h1>
+    );
   }
+}
